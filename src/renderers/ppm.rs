32e5fb8f291dd2ee764690bfaa6478ea317bcac6
Filip Trplan <info@trplan.si>
2025-08-01 11:23:31 +0200

    feat: ppm renderer progress bar

--- src/renderers/ppm.rs
+++ src/renderers/ppm.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+use indicatif::ProgressBar;
+
 #[derive(Default)]
 pub struct PPM {}
 
@@ -15,16 +17,21 @@ impl super::Renderer for PPM {
     fn draw(self, image: super::Image) {
         let mut buffer = String::new();
 
+        let bar = ProgressBar::new(image.height as u64);
+
         // Header
         buffer += format!("P3\n{} {}\n255\n", image.width, image.height).as_str();
 
         for y in 0..image.height {
+            bar.inc(1);
             for x in 0..image.width {
                 let color = image.get_pixel(x, y);
                 buffer += format!("{} {} {} ", color.r, color.g, color.b).as_str();
             }
             buffer += "\n";
         }
+
+        bar.finish();
 
         if let Err(e) = fs::write(OUTPUT_FILE, buffer) {
             panic!("Error writing to file: {}", e);
